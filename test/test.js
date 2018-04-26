00b31cccce672c6f47e6ddfbc488921c831bf2e1
David Graham <david.malcom.graham@gmail.com>
2018-04-26 11:58:45 -0600

    Test check event dispatch

--- test/test.js
+++ test/test.js
@@ -11,4 +11,53 @@ describe('task-lists element', function() {
       assert.equal('TASK-LISTS', el.nodeName)
     })
   })
+
+  describe('selecting checkboxes', function() {
+    beforeEach(function() {
+      const container = document.createElement('div')
+      container.innerHTML = `
+        <task-lists>
+          <ul class="contains-task-list">
+            <li class="task-list-item">
+              <input type="checkbox" class="task-list-item-checkbox"> Hubot
+            </li>
+            <li class="task-list-item">
+              <input type="checkbox" class="task-list-item-checkbox"> Bender
+            </li>
+          </ul>
+
+          <ul class="contains-task-list">
+            <li class="task-list-item">
+              <input type="checkbox" class="task-list-item-checkbox"> BB-8
+            </li>
+            <li class="task-list-item">
+              <input id="wall-e" type="checkbox" class="task-list-item-checkbox"> WALL-E
+            </li>
+          </ul>
+        </task-lists>`
+      document.body.append(container)
+    })
+
+    afterEach(function() {
+      document.body.innerHTML = ''
+    })
+
+    it('emits check event with position', function() {
+      let called = false
+
+      const list = document.querySelector('task-lists')
+      list.addEventListener('task-lists:check', function(event) {
+        called = true
+        const {position, checked} = event.detail
+        assert.deepEqual(position, [1, 1])
+        assert(checked)
+      })
+
+      const checkbox = document.querySelector('#wall-e')
+      checkbox.checked = true
+      checkbox.dispatchEvent(new CustomEvent('change', {bubbles: true}))
+
+      assert(called)
+    })
+  })
 })
