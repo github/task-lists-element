3a4872f0fa96a03f6a72f9a600855d07a39f1075
iulia bejan <iulia-b@github.com>
2021-02-15 13:00:36 +0100

    Add missing attrs

--- src/task-lists-element.ts
+++ src/task-lists-element.ts
@@ -82,7 +82,7 @@ export default class TaskListsElement extends HTMLElement {
 const handleTemplate = document.createElement('template')
 handleTemplate.innerHTML = `
   <span class="handle">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+    <svg xmlns="http://www.w3.org/2000/svg" class="drag-handle" aria-hidden="true" viewBox="0 0 16 16" width="16" height="16">
       <path fill-rule="evenodd" d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z"></path>
     </svg>
   </span>`
