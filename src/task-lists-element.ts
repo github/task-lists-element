a50a3e1dcb58c2798c1dfdb56dca0a4447622ad9
Iulia Bejan <64602043+iulia-b@users.noreply.github.com>
2021-02-16 10:18:25 +0100

    Update src/task-lists-element.ts
    Co-authored-by: Kristján Oddsson <koddsson@gmail.com>

--- src/task-lists-element.ts
+++ src/task-lists-element.ts
@@ -82,8 +82,8 @@ export default class TaskListsElement extends HTMLElement {
 const handleTemplate = document.createElement('template')
 handleTemplate.innerHTML = `
   <span class="handle">
-    <svg xmlns="http://www.w3.org/2000/svg" class="drag-handle" aria-hidden="true" viewBox="0 0 16 16" width="16" height="16">
-      <path fill-rule="evenodd" d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z"></path>
+    <svg class="drag-handle" aria-hidden="true" width="16" height="16">
+      <path d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z"/>
     </svg>
   </span>`
 
